475a238c74beb460070b010c86b17bfe10b41bf4
Nicole Ho <nicolezxho@gmail.com>
2022-05-22 19:05:32 +1000

    Toggle channel size

--- assets/js/aredotna.js
+++ assets/js/aredotna.js
@@ -17,9 +17,11 @@ window.onload = function () {
       authorEl.innerHTML = "Channel by " + author;
 
       channel.contents.forEach((blockParent) => {
+        console.log(blockParent);
         const contentsParentEl = document.querySelector("div.content");
         const channel = document.createElement("div");
         channel.classList.add("channel");
+        channel.dataset.toggled = false;
         contentsParentEl.appendChild(channel);
 
         if (blockParent.class === "Channel") {
@@ -27,14 +29,52 @@ window.onload = function () {
           blockParentInfoEl.classList.add("block-info");
           channel.appendChild(blockParentInfoEl);
 
+          const blockParentInfoTop = document.createElement("div");
+          blockParentInfoTop.classList.add("block-info-top");
+          const blockParentInfoBottom = document.createElement("div");
+          blockParentInfoBottom.classList.add("block-info-bottom");
+          blockParentInfoEl.append(blockParentInfoTop, blockParentInfoBottom);
+
+          const blockParentContentEl = document.createElement("div");
+          blockParentContentEl.classList.add("block-content");
+          channel.appendChild(blockParentContentEl);
+
           const blockParentTitle = blockParent.title;
           const blockParentTitleEl = document.createElement("h3");
           blockParentTitleEl.innerHTML = blockParentTitle;
-          blockParentInfoEl.appendChild(blockParentTitleEl);
+
           const blockParentMeta = blockParent.metadata.description;
-          const blockParentMetaEl = document.createElement("p");
+          const blockParentMetaEl = document.createElement("div");
+          blockParentMetaEl.classList.add("description");
           blockParentMetaEl.innerHTML = blockParentMeta;
-          blockParentInfoEl.appendChild(blockParentMetaEl);
+
+          blockParentInfoTop.append(blockParentTitleEl, blockParentMetaEl);
+
+          const blockParentInputEl = document.createElement("input");
+          blockParentInputEl.type = "checkbox";
+          blockParentInputEl.name = "expand-" + blockParentTitle;
+          blockParentInputEl.id = "expand-" + blockParentTitle;
+
+          const blockParentLabelEl = document.createElement("label");
+          blockParentLabelEl.innerHTML = "[+] show more";
+          blockParentLabelEl.setAttribute("for", "expand-" + blockParentTitle);
+
+          blockParentInfoBottom.append(blockParentInputEl, blockParentLabelEl);
+
+          blockParentInputEl.addEventListener("input", function () {
+            const thisLabel = this.nextSibling;
+            const thisChannel = this.closest("div.channel");
+            // console.log(this.checked);
+            if (this.checked) {
+              console.log("checked");
+              thisChannel.dataset.toggled = "true";
+              thisLabel.innerHTML = "[-] show less";
+            } else {
+              console.log("unchecked");
+              thisChannel.dataset.toggled = "false";
+              thisLabel.innerHTML = "[+] show more";
+            }
+          });
 
           const blockParentSlug = blockParent.slug;
           fetch(`https://api.are.na/v2/channels/${blockParentSlug}/contents`)
@@ -42,17 +82,17 @@ window.onload = function () {
               return response.json();
             })
             .then((blockChildren) => {
-              console.log(blockChildren);
+              // console.log(blockChildren);
 
               blockChildren.contents.forEach((block) => {
-                console.log(block);
+                // console.log(block);
 
                 const blockThumb = block.image.original.url;
                 const blockThumbEl = document.createElement("img");
                 const blockThumbAlt = block.title;
                 blockThumbEl.src = blockThumb;
                 blockThumbEl.alt = blockThumbAlt;
-                channel.appendChild(blockThumbEl);
+                blockParentContentEl.appendChild(blockThumbEl);
               });
             });
         }
@@ -62,49 +102,3 @@ window.onload = function () {
       console.log(error);
     });
 };
-
-// Fetch channel contents
-// fetch(`https://api.are.na/v2/channels/${channelSlug}/contents`)
-//   .then((reponse) => {
-//     return reponse.json();
-//   })
-//   .then((contents) => {
-//     // console.log(contents);
-
-//     for (let i = 0; i < contents.length; i++) {
-//       // Runs 5 times, with values of step 0 through 4.
-//       // console.log(contents.class);
-//     }
-
-//     // if (contents.class === "Channel") {
-//     // }
-//   })
-//   .catch((error) => {});
-
-// const Arena = require("are.na");
-
-// const arena = new Arena();
-
-// arena
-//   .channel("arena-influences")
-//   .get()
-//   .then((chan) => {
-//     chan.contents.map((item) => {
-//       console.log(item.title);
-//     });
-//   })
-//   .catch((err) => console.log(err));
-
-// // const Arena = require("are.na");
-// // const arena = new Arena({
-// //   accessToken: "Z-d03dcdyQ5QGeuL6AWyXHiIhD2hq7APCOtedS23Ai8",
-// // });
-
-// // arena
-// //   .channel("xxx-hot-faces-in-your-area-xxx")
-// //   .get()
-// //   .then((chan) => {
-// //     console.log(chan);
-// //     // return chan.json();
-// //   })
-// //   .catch((error) => console.log(error));
